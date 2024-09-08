a525cb05b7e9eb7e38ade4617857349123795ca6
Cory Pease <coryrpease@gmail.com>
2024-09-07 20:14:34 -0400

    hello world python app

---
+++ hello.py
@@ -0,0 +1,10 @@
+# hello.py
+
+# This is a simple Python script that prints "Hello, World!" to the console
+def main():
+    print("Hello, Anaconda!")
+
+# This ensures the script runs when executed
+if __name__ == "__main__":
+    main()
+
